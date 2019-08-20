70c12718432025477cb3567cb2f48d5efdbd7c55
Ricardo Oliveira <ricardo@hashicorp.com>
2019-08-20 12:19:28 +0100

    fising whitespace and adding tls cert to vault

--- modules/vault-lb.tf
+++ modules/vault-lb.tf
@@ -6,10 +6,10 @@ resource "aws_alb" "vault" {
 
   tags = {
     Name           = "${var.namespace}-vault"
-    owner          =var.owner
-   created-by     = "${var.created-by}"
+    owner          = var.owner
+    created-by     = "${var.created-by}"
     sleep-at-night = "${var.sleep-at-night}"
-    TTL            =var.TTL
+    TTL            = var.TTL
  }
 }
 
@@ -32,20 +32,26 @@ resource "aws_alb_target_group" "vault" {
 }
 
 resource "aws_alb_listener" "vault" {
+  depends_on = [
+    aws_acm_certificate_validation.cert
+  ]
+
   load_balancer_arn = "${aws_alb.vault.arn}"
 
-  port     = "8200"
-  protocol = "HTTP"
-
+  port            = "8200"
+  protocol        = "HTTPS"
+  certificate_arn = "${aws_acm_certificate_validation.cert.certificate_arn}"
+  ssl_policy      = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
   default_action {
     target_group_arn = "${aws_alb_target_group.vault.arn}"
     type             = "forward"
   }
+
 }
 
 resource "aws_alb_target_group_attachment" "vault" {
-  count            =var.servers
- target_group_arn = "${aws_alb_target_group.vault.arn}"
+  count            = var.servers
+  target_group_arn = "${aws_alb_target_group.vault.arn}"
   target_id        = "${element(aws_instance.server.*.id, count.index)}"
   port             = "8200"
 }
