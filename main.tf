e3bad40dc6bc5140c1ada1dd680aaf909f68b68a
Guy Barros <guy@hashicorp.com>
2019-12-17 16:46:24 +0000

    added backend config file and example, updated gitignore

--- main.tf
+++ main.tf
@@ -2,12 +2,7 @@
 # Using a single workspace:
 terraform {
   backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "emea-se-playground-2019"
-
-    workspaces {
-      name = "Guy-AWS-Demostack"
-    }
+   
   }
 }
 
