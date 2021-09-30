f4928447e0afaa761dd5bbddc625990001127925
Guy Barros <guy@hashicorp.com>
2021-09-30 18:01:30 +0100

    forgot the vars file

--- variables.tf
+++ variables.tf
@@ -63,44 +63,14 @@ variable "workers" {
   default     = "3"
 }
 
-variable "consul_url" {
-  description = "The url to download Consul."
-  default     = "https://releases.hashicorp.com/consul/1.2.2/consul_1.2.2_linux_amd64.zip"
-}
-
-variable "consul_ent_url" {
-  description = "The url to download Consul."
-  default     = "https://releases.hashicorp.com/consul/1.2.2/consul_1.2.2_linux_amd64.zip"
-}
-
 variable "fabio_url" {
   description = "The url download fabio."
   default     = "https://github.com/fabiolb/fabio/releases/download/v1.5.7/fabio-1.5.7-go1.9.2-linux_amd64"
 }
 
-variable "nomad_url" {
-  description = "The url to download nomad."
-  default     = "https://releases.hashicorp.com/nomad/0.8.4/nomad_0.8.4_linux_amd64.zip"
-}
-
-variable "nomad_ent_url" {
-  description = "The url to download nomad."
-  default     = "https://releases.hashicorp.com/nomad/0.8.4/nomad_0.8.4_linux_amd64.zip"
-}
-
 variable "cni_plugin_url" {
   description = "The url to download teh CNI plugin for nomad."
   default     = "https://github.com/containernetworking/plugins/releases/download/v0.8.2/cni-plugins-linux-amd64-v0.8.2.tgz"
-}
-
-variable "vault_url" {
-  description = "The url to download vault."
-  default     = "https://releases.hashicorp.com/vault/0.11.1/vault_0.11.1_linux_amd64.zip"
-}
-
-variable "vault_ent_url" {
-  description = "The url to download vault."
-  default     = "https://s3-us-west-2.amazonaws.com/hc-enterprise-binaries/vault/ent/0.11.1/vault-enterprise_0.11.1%2Bent_linux_amd64.zip"
 }
 
 variable "owner" {
